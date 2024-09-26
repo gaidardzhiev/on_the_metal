2e63050a05a36109da3eec29291e72445764c99d
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-09-26 18:55:12 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c start.S -o start.o
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -std=gnu99 -c main.c -o main.o -O2 -Wall -Wextra            arm-none-eabi-gcc -T link.ld -o on_the_metal.elf -ffreestanding -O2 -nostdlib start.o main.o -lgcc                    arm-none-eabi-objcopy on_the_metal.elf -O binary kernel7.img
+qemu-system-aarch64 -M raspi0 -serial stdio -kernel kernel7.img
