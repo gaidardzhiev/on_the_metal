aa7c55db4f99421643f9c75c26a0c0312ebaa8d6
gaidardzhiev <1v4n@protonmail.com>
2025-05-25 09:20:28 +0000

    Sun May 25 09:20:28 AM UTC 2025

---
+++ lisp/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+IMG="kernel7.img"
+
+[ ! -f "$IMG" ] && {
+	printf "$IMG not found...\n";
+	exit 1;
+}
+
+qemu-system-arm \
+  -M raspi \
+  -cpu arm1176 \
+  -m 256 \
+  -kernel "$IMG" \
+  -serial stdio \
+  -display sdl \
+  -no-reboot
